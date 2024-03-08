837bba34f3708763236f01fac3e8cbdbfe3fa466
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-08 15:32:10 +0100

    [add] js-ESERCIZIO 1

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -4,16 +4,31 @@
     1- Che cosa fa questo codice?
     2- Sono presenti errori di sintassi?
     3- Sono presenti errori logici?
-
     Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
 
 *******************************************************************************/
 
-
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i <= 5; i++) {
     console.log(i);
 }
+
+/*
+    1- Che cosa fa questo codice?
+
+        // ESERCIZIO 1
+        il codice fornito realizza un ciclo fino a quando la seconda condizione non resulterà falsa. Dandogli come condizione iniziale la variabile let i = 0, la seconda condizione sarà i < 5, essa conntrollerà se il valore i è inferiore a 5, i++ incrementa il valore di i a ogni ciclo. Quando il valore di ì non sarà più inferiore a 5 il ciclo termina
+
+     2- Sono presenti errori di sintassi?
+
+        // ESERCIZIO 1
+        si, il simbolo nella seconda condizione dovrebbe essere inferiore e/o uguale a 
+
+    3- Sono presenti errori logici?
+        // ESERCIZIO 1
+        si, la condizione i > 5 è semrpre falsa, perchè la prima condizione è i = 0, quindi il ciclo non viene eseguito
+
+*/
 
 
 // ESERCIZIO 2
